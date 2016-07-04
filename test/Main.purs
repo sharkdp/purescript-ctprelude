ed452331a0f69a2faa1dfd76e221374a29da4307
sharkdp <davidpeter@web.de>
2016-07-05 00:45:20 +0200

    Prove that a * a = 2 * a

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,7 @@ module Test.Main where
 
 import CTPrelude
 
--- | Prove that `Bool` is isomorphic to `One ⊕ One`.
+-- Prove that `Bool` is isomorphic to `One ⊕ One`.
 data Bool = True | False
 
 instance i0 :: Isomorphism0 Bool (One ⊕ One) where
@@ -12,10 +12,20 @@ instance i0 :: Isomorphism0 Bool (One ⊕ One) where
   bwd0 (CoproductA One) = True
   bwd0 (CoproductB One) = False
 
--- | Prove that `Maybe` is isomorphic to `Const One ⊕ Identity`.
+-- Prove that `a ⊕ a` is isomorphic to `Two ⊗ a`
+newtype TwoTimes a = TwoTimes (Two ⊗ a)
+
+instance i1 :: Isomorphism0 (a ⊕ a) (TwoTimes a) where
+  fwd0 (CoproductA x) = TwoTimes $ TwoA ⊗ x
+  fwd0 (CoproductB y) = TwoTimes $ TwoB ⊗ y
+
+  bwd0 (TwoTimes (TwoA ⊗ x)) = CoproductA x
+  bwd0 (TwoTimes (TwoB ⊗ y)) = CoproductB y
+
+-- Prove that `Maybe` is isomorphic to `Const One ⊕ Identity`.
 data Maybe a = Nothing | Just a
 
-instance i1 :: Isomorphism1 Maybe (Const One ⊞ Identity) where
+instance i2 :: Isomorphism1 Maybe (Const One ⊞ Identity) where
   fwd1 Nothing = CoproductF $ CoproductA (Const One)
   fwd1 (Just x) = CoproductF $ CoproductB (Identity x)
 
